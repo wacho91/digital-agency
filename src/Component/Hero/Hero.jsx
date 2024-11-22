da5025e56c926b2954c7dcdd46da1b75b0849832
wacho91 <criskol.71@gmail.com>
2024-11-22 10:35:24 -0500

    Arquitecture  hero component

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,9 +1,43 @@
 
+import YellowCar from "../../assets/website/team.png";
 
 const Hero = () => {
   return (
     <div>
-        Hero Component
+        <div>
+            <div>
+                {/* Image section */}
+                <div>
+                    <img 
+                        src={YellowCar} 
+                        alt="Image" 
+                        className="w-full sm:max-w-[280px] md:max-w-[430px]"
+                    />
+                    <div>
+                        <p>⭐Projects</p>
+                        <h1>
+                            600+ <span>Done</span>                        
+                        </h1>
+                    </div>
+                </div>
+                {/* Text section */}
+                <div>
+                    <h1>
+                        Building Brands in the{" "}
+                        <span>Digital Agency</span>                   
+                    </h1>
+                    <p>
+                        Your partner in navigating the ever-evolving landscape of digital
+                        marketing. From conceptualization to execution, we craft tailored
+                        solutions that drive results and elevate your brand to new
+                        heights.
+                    </p>
+                    <button>
+                        Learn More
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
