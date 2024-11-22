1dbd26d70d65225c692c79899a1887ce671019d6
wacho91 <criskol.71@gmail.com>
2024-11-22 10:46:47 -0500

    Hero Complete

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -3,28 +3,28 @@ import YellowCar from "../../assets/website/team.png";
 
 const Hero = () => {
   return (
-    <div>
-        <div>
-            <div>
+    <div className="dark:bg-gray-950 dark:text-white duration-300">
+        <div className="container min-h-[620px] flex mt-10 sm:mt-0">
+            <div  className="grid grid-cols-1 sm:grid-cols-2 gap-6 place-items-center">
                 {/* Image section */}
-                <div>
+                <div className="order-1 sm:order-2 relative">
                     <img 
                         src={YellowCar} 
                         alt="Image" 
                         className="w-full sm:max-w-[280px] md:max-w-[430px]"
                     />
-                    <div>
-                        <p>⭐Projects</p>
-                        <h1>
-                            600+ <span>Done</span>                        
+                    <div  className="absolute -bottom-5 -right-8 px-4 py-2 rounded-xl bg-white dark:bg-gray-900 dark:text-white shadow-md">
+                        <p className="text-gray-500 text-sm">⭐Projects</p>
+                        <h1 className="font-bold">
+                            600+ <span className="font-normal">Done</span>                        
                         </h1>
                     </div>
                 </div>
                 {/* Text section */}
-                <div>
-                    <h1>
+                <div className="space-y-5 order-2 sm:order-1 xl:pr-40 ">
+                    <h1 className="text-4xl sm:text-5xl font-semibold">
                         Building Brands in the{" "}
-                        <span>Digital Agency</span>                   
+                        <span className="text-primary">Digital Agency</span>                   
                     </h1>
                     <p>
                         Your partner in navigating the ever-evolving landscape of digital
@@ -32,7 +32,7 @@ const Hero = () => {
                         solutions that drive results and elevate your brand to new
                         heights.
                     </p>
-                    <button>
+                    <button className="primary-btn">
                         Learn More
                     </button>
                 </div>
