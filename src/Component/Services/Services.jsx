7db96c65a75e43dcbec35018107a5878086c045c
wacho91 <criskol.71@gmail.com>
2024-11-22 11:41:07 -0500

    Services Component Complete

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -43,30 +43,36 @@ const Services = () => {
   return (
     <>
         <span id="about"></span>
-        <div>
-            <div>
+        <div className="bg-gray-100 dark:bg-black dark:text-white py-12 sm:grid sm:place-items-center">
+            <div className="container">
                 {/* Header */}
-                <div>
-                    <h1>
+                <div className="pb-12 text-center space-y-3">
+                    <h1 className="text-3xl font-semibold sm:text-3xl text-violet-950 dark:text-primary">
                         Explore Our Services
                     </h1>
-                    <p>
+                    <p className="text-gray-600 dark:text-gray-400 text-sm">
                         We are self-service data analytics software that lets you create
                         visually.
                     </p>
                 </div>
                 {/* services cards */}
-                <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
                     {skillsData.map((skill) => (
-                        <div key={skill.nsme}>
+                        <div 
+                            key={skill.nsme}
+                            className="card space-y-3 sm:space-y-4 p-4"
+                        >
                             <div>{skill.icon}</div>
-                            <p>{skill.description}</p>
+                            <h1 className="text-lg font-semibold">{skill.name}</h1>
+                            <p className="text-gray-600 dark:text-gray-400">
+                                {skill.description}
+                            </p>
                         </div>
                     ))}
                 </div>
                 {/* button */}
-                <div>
-                    <button>
+                <div className="text-center mt-4 sm:mt-8">
+                    <button className="primary-btn">
                         Learn More
                     </button>
                 </div>
