93c210348c785cf5d9bb8c8340664afab322112e
wacho91 <criskol.71@gmail.com>
2024-11-25 11:42:20 -0500

    BlogComp Component Complete

--- src/Component/BlogsComp/BlogsComp.jsx
+++ src/Component/BlogsComp/BlogsComp.jsx
@@ -38,18 +38,18 @@ const BlogsData = [
 const BlogsComp = () => {
   return (
     <>
-        <div>
-            <section>
-                <h1>
+        <div className="bg-gray-100 dark:bg-gray-900 dark:text-white py-10 pb-14">
+            <section className="container">
+                <h1 className="my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-semibold">
                     Our Blogs
                 </h1>
-                <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {BlogsData.map((item) => (
                         <BlogCard key={item.id} {...item} />
                     ))}
                 </div>
-                <div>
-                    <button>View All Posts</button>
+                <div className="text-center">
+                    <button className="primary-btn">View All Posts</button>
                 </div>
             </section>
         </div>
