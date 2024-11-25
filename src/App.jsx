14949730f4f6eb35c6541b6262e6881db09b6d20
wacho91 <criskol.71@gmail.com>
2024-11-25 11:45:24 -0500

    Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import BrandsLogo from "./Component/BrandsLogo/BrandsLogo";
 import Services from "./Component/Services/Services";
 import Testimonial from "./Component/Testimonial/Testimonial";
 import BlogsComp from "./Component/BlogsComp/BlogsComp";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
         <Services />
         <Testimonial />
         <BlogsComp />
+        <Footer />
     </div>
   )
 }
---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div>
+        Footer Component
+    </div>
+  )
+}
+
+export default Footer
